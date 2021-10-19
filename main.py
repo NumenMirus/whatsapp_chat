f669080a91bc6be443e232cc84cc989b50877534
NumenMirus <michaelbastauni@gmail.com>
2021-10-19 18:28:08 +0200

    better graphic generator

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import pandas as pd
 import csv
+import docx
+from docx.oxml.shared import OxmlElement
+from docx.oxml.ns import qn
 from docx import Document
 from docx.text.paragraph import Paragraph
 from docx.enum.style import WD_STYLE_TYPE
 from docx.shared import Pt
+from docx.shared import RGBColor
+from docx.oxml.ns import qn
+from docx.shared import Mm
+from docx.oxml.shared import OxmlElement
 from tqdm import tqdm
 import os
 
@@ -28,40 +35,126 @@ def _convert_to_csv(filename):
     df['Text'] = df['Text'].str.replace('Questo messaggio è stato eliminato','Messaggio Eliminato')    
     df.to_csv("chat.csv",index=False)
 
+def _create_mike_paragraph(d, text, alignment):
+    # Add a paragraph
+    p = d.add_paragraph()
+    p.alignment = 2
+    p.paragraph_format.line_spacing = 1.5
+
+    #Paragraph format
+    paragraph_format = p.paragraph_format
+    paragraph_format.line_spacing = Pt(15)
+    paragraph_format.space_after = Pt(3)
+
+    # Add text to paragraph reference
+    txt = text
+    run = p.add_run(txt)
+
+    # Get the XML tag
+    tag = run._r
+
+    # Create XML element
+    shd = OxmlElement('w:shd')
+
+    # Add attributes to the element
+    shd.set(qn('w:val'), 'clear')
+    shd.set(qn('w:color'), 'auto')
+    shd.set(qn('w:fill'), '00CC33')
+
+    # Set the font size - this is important! Without this step the
+    # tag.rPr value below will be None.
+    run.font.size = Pt(10)
+    run.font.name = 'Ubuntu'
+
+    tag.rPr.append(shd)
+
+def _create_giorgia_paragraph(d, text, alignment):
+    # Add a paragraph
+    p = d.add_paragraph()
+    p.alignment = 0
+    p.paragraph_format.line_spacing = 1.5
+
+    #Paragraph format
+    paragraph_format = p.paragraph_format
+    paragraph_format.line_spacing = Pt(15)
+    paragraph_format.space_after = Pt(3)
+
+    # Add text to paragraph reference
+    txt = text
+    run = p.add_run(txt)
+
+    # Get the XML tag
+    tag = run._r
+
+    # Create XML element
+    shd = OxmlElement('w:shd')
+
+    # Add attributes to the element
+    shd.set(qn('w:val'), 'clear')
+    shd.set(qn('w:color'), 'auto')
+    shd.set(qn('w:fill'), 'FFFFFF')
+
+    # Set the font size - this is important! Without this step the
+    # tag.rPr value below will be None.
+    run.font.size = Pt(10)
+    run.font.name = 'Ubuntu'
+
+    tag.rPr.append(shd)
+
 def _create_document():
     d = Document()
+    document = Document()
+    section = d.sections[0]
+    section.page_height = Mm(297)
+    section.page_width = Mm(210)
+    section.left_margin = Mm(50)
+    section.right_margin = Mm(50)
+    section.top_margin = Mm(25.4)
+    section.bottom_margin = Mm(25.4)
+    section.header_distance = Mm(12.7)
+    section.footer_distance = Mm(12.7)
 
-    #creating the style
-    styles = d.styles
-    style = styles.add_style('Arial', WD_STYLE_TYPE.PARAGRAPH) #Tahoma is the name I set because that's the font I'm gonna use
-    style.font.name = 'Arial'
-    style.font.size = Pt(11)
+    # Now Add below children to root xml tree
+    # create xml element using OxmlElement
+    shd = OxmlElement('w:background')
+    # Add attributes to the xml element
+    shd.set(qn('w:color'), 'F5F4EF') #black color
+    shd.set(qn('w:themeColor'), 'text1')
+    shd.set(qn('w:themeTint'), 'F2')
+    # Add background element at the start of Document.xml using below
+    d.element.insert(0,shd)
+    # Add displayBackgroundShape element to setting.xml
+    shd1 = OxmlElement('w:displayBackgroundShape')
+    d.settings.element.insert(0,shd1)
+
+    # #creating the style for Mike
+    # styles = d.styles
+    # style = styles.add_style('Arial_mike', WD_STYLE_TYPE.PARAGRAPH) #Arial is the name I set because that's the font I'm gonna use
+    # style.font.name = 'Arial'
+    # style.font.size = Pt(8)
+    # style.font.color.rgb = RGBColor(0x72, 0xCC, 0x48)
+
+    # #creating the style for Giorgia
+    # styles = d.styles
+    # style = styles.add_style('Arial_giorgia', WD_STYLE_TYPE.PARAGRAPH) #Arial is the name I set because that's the font I'm gonna use
+    # style.font.name = 'Arial'
+    # style.font.size = Pt(8)
+    # style.font.color.rgb = RGBColor(0x28, 0x54, 0xD5)
 
     #composing the document
     d.add_heading('Chat con Potato', 0)
-    p = d.add_paragraph()
-    p.style = d.styles['Arial']
-    p.add_run('Giorgia').bold = True
-    p.add_run('\t\t\t\t\t\t\t\t\t\t')
-    p.add_run('Mike').bold = True
 
     file = csv.reader(open('chat.csv', 'r'))
 
     i = 0
     for line in tqdm(file, desc="Parsing messages..."):
-        if line[3] == " Mike":
-            p = d.add_paragraph()
-            p.paragraph_format.line_spacing = 0.5
-            p.style = d.styles['Arial']
-            p.alignment = 2
-            p.add_run(line[2])
+        if line[3] == ' Mike':
+            _create_mike_paragraph(d, line[2]+line[1], 2)
         elif line[3] == ' GiorgiaChiarucci':
-            p = d.add_paragraph()
-            p.paragraph_format.line_spacing = 0.5
-            p.style = d.styles['Arial']
-            p.alignment = 0
-            p.add_run(line[2])
+            _create_giorgia_paragraph(d,  line[1]+line[2], 0)
         i = i+1
+
+        #temporary limit in the loop for testing purposes
         if i == 1000:
             break
 
@@ -71,6 +164,8 @@ if not os.path.isfile('./chat.csv'):
     if os.path.isfile('./chat.txt'):
         print('CSV file not found, converting chat.txt in chat.csv..')
         _convert_to_csv(filename)
+        print('\nAll ok now, creating document...\n')
+        _create_document()
     else:
         print('Cannot find chat.txt nor chat.csv in folder, please add the chat source')
 else:
